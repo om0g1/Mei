b5b7a654e9ad112e6d23c5d6098fefc5fe4d9288
om0g1 <omogidavis@gmail.com>
2023-07-30 16:09:05 +0300

    v0.0.1

--- mei.cpp
+++ mei.cpp
@@ -169,7 +169,7 @@ int main () {
         try {
             std::cout << ">>> " << interpreter.interprete() << std::endl;
         } catch (std::exception& e) {
-            std::cout << "Error: " << e.what() << std::endl;
+            std::cout << "Error " << e.what() << std::endl;
         }
     }
 };
